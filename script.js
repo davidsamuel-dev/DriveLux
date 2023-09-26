ee20053405d3b7c67aec233f92413c14f09eaa6a
davidsamuel-dev <davidsamuel.dev@gmail.com>
2023-09-26 10:58:35 -0300

    atualização mais recente

--- script.js
+++ script.js
@@ -1,11 +1,8 @@
 var caixa_menu = document.getElementsByTagName("aside")[0]
 
-
-
 function menu() {
  
     caixa_menu.style.top = "0"
-    // caixa_menu.style.width = "100%"
     caixa_menu.style.display = "inline-block"
 
 }
@@ -14,6 +11,6 @@ function voltar_menu() {
 
    
     caixa_menu.style.top = "-410px";
-    // caixa_menu.style.width = "300px";;
+
     
 }
